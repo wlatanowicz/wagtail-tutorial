2fd5ea85764bf1d8bb8244c032e8b5a35d7a894e
Wiktor Latanowicz <wiktor@latanowicz.com>
2018-09-11 22:31:20 +0200

    Add StreamField

--- demosite/home/models.py
+++ demosite/home/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from wagtail.core import blocks
 
 from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import RichTextField, StreamField
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.api import APIField
+from wagtail.images.blocks import ImageChooserBlock
 
 
 class HomePage(Page):
@@ -15,4 +17,26 @@ class HomePage(Page):
 
     api_fields = [
         APIField('body'),
+    ]
+
+
+class AnotherPage(Page):
+    sf_body = StreamField([
+        ('heading', blocks.CharBlock(classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+        ('person', blocks.StructBlock([
+            ('first_name', blocks.CharBlock()),
+            ('surname', blocks.CharBlock()),
+            ('photo', ImageChooserBlock(required=False)),
+            ('biography', blocks.RichTextBlock()),
+        ], icon='user'))
+    ], blank=True)
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('sf_body'),
+    ]
+
+    api_fields = [
+        APIField('sf_body'),
     ]
